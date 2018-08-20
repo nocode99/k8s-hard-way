1d9dcc86db265fec92957f6d5df12cefa1b03fbb
nocode99 <nocode99@gmail.com>
2018-08-19 20:22:15 -0400

    added dependency to subnetwork

--- gcp/network.tf
+++ gcp/network.tf
@@ -70,4 +70,6 @@ resource "google_compute_route" "kubernetes_worker" {
   network     = "${google_compute_network.kubernetes.name}"
   next_hop_ip = "${format("10.240.0.2%d", count.index)}"
   dest_range  = "${format("10.200.%d.0/24", count.index)}"
+
+  depends_on = ["google_compute_subnetwork.kubernetes"]
 }
